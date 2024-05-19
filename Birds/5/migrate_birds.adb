0ef7158fdd49ca6da8dc122b6c470f502f9dfa1d
Jamal Mammadov <91668167+jamuall@users.noreply.github.com>
2024-05-19 20:15:07 +0200

    Update migrate_birds.adb

--- Birds/5/migrate_birds.adb
+++ Birds/5/migrate_birds.adb
@@ -1,18 +1,17 @@
 procedure migrate_birds(G: in out Grid; Max: Natural) is
-   	Temp : Grid := G;
 begin
    for I in G'Range(1) loop
       for J in G'Range(2) loop
          if G(I,J) >= Max then
             if Index'Pred(I) in G'Range(1) then
-               G(Index'Pred(I),J) := Temp(Index'Pred(I),J)  + 1; end if;
+               G(Index'Pred(I),J) := G(Index'Pred(I),J) + 1; end if;
             if Index'Succ(I) in G'Range(1) then
-               G(Index'Succ(I), J) := Temp(Index'Succ(I), J) + 1; end if;
+               G(Index'Succ(I), J) := G(Index'Succ(I), J) + 1; end if;
             if Index'Pred(J) in G'Range(2) then
-               G(I,Index'Pred(J)) := Temp(I,Index'Pred(J)) + 1; end if;
+               G(I,Index'Pred(J)) := G(I,Index'Pred(J)) + 1; end if;
             if Index'Succ(J)  in G'Range(2) then
-               G(I, Index'Succ(J)) := Temp(I, Index'Succ(J)) + 1; end if;
-            G(I,J) := Temp(I, J) - 4;
+               G(I, Index'Succ(J)) := G(I, Index'Succ(J)) + 1; end if;
+            G(I,J) := G(I, J) - 4;
          end if;
       end loop;
    end loop;
