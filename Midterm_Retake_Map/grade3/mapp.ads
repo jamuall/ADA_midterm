80bf614a6e1288c560cbc782d4675bc173d95904
Jamal Mammadov <91668167+jamuall@users.noreply.github.com>
2024-11-05 23:39:17 +0100

    Create mapp.ads

---
+++ Midterm_Retake_Map/grade3/mapp.ads
@@ -0,0 +1,31 @@
+generic
+   type Key is (<>);
+   type Value is (<>);
+
+
+package mapP is
+
+   type Map(Max: Natural) is private;
+
+   function Size(M: Map) return Natural;
+   function Contains(M: Map; K:Key) return Boolean;
+   procedure Put(M: in out Map; K:Key; V: Value);
+   function Get(M: Map; K: Key) return Value;
+   
+   procedure Remove(M: in out Map; K: Key);
+   
+private
+   
+   type Pair is record
+      PKey: Key;
+      PValue: Value;
+   end record;
+ 
+   type m_array is array (Natural range <>) of Pair;
+   
+   type Map(Max: Natural) is record 
+      Data: m_array(0..Max);
+      Pointer: Natural := 0;
+   end record;
+   
+end mapP;
